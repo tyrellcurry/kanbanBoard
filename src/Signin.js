cba1137d2d9073a813f8a087db69cef9be1e3260
Tyrell C <tyrellrcurry@gmail.com>
2024-05-16 15:40:36 -0700

    Configured React, Webpack, Babel, and SCSS

---
+++ src/Signin.js
@@ -0,0 +1,10 @@
+// src/Signin.js
+import React from "react";
+
+const Signin = () => {
+  return (
+    <h1>This is the sign in component</h1>
+  )
+}
+
+export default Signin;
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.join(__dirname, "src", "index.js"),
+    output: {
+      path:path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, "public", "index.html"),
+      }),
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.?js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: ['@babel/preset-env', '@babel/preset-react']
+              }
+            }
+          },
+          {
+            test: /\.(scss|css)$/,
+            use: [
+              // Creates `style` nodes from JS strings
+              "style-loader",
+              // Translates CSS into CommonJS
+              "css-loader",
+              // Compiles Sass to CSS
+              "sass-loader",
+            ],
+          },
+        ]
+      },
+  devServer: {
+    port: 3000,
+  },
+};
