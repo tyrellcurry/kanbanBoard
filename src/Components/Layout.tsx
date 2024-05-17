d828ef67d06a19f44de43292262e982dea153f2c
Tyrell C <tyrellrcurry@gmail.com>
2024-05-16 20:28:48 -0700

    Layout base added

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,21 +1,29 @@
 // src/Layout.tsx
 import React from "react";
-import '../../src/app.scss';
+import "../../src/app.scss";
 
-const Layout = ({children}: any) => {
+const Layout = ({ children }: any) => {
   return (
-  <div className="layout">
-    <nav>
-      <div><img src="./images/logo-1.png" alt="" /></div>
-      <ul>
-        <li>
-          <button>Sign In</button>
-        </li>
-      </ul>
-    </nav>
-    <h1>I am the layout</h1>
-    {children}
-  </div>
-  )};
+    <div className="layout">
+      <nav>
+        <div className="logo">
+          <a href="/">
+            <img src="./images/logo-1.png" alt="" />
+            Tira
+          </a>
+        </div>
+        <ul>
+          <li>
+            <button>Sign In</button>
+          </li>
+        </ul>
+      </nav>
+      <main>{children}</main>
+      <footer>
+        Copyright &copy; Tira 2024
+      </footer>
+    </div>
+  );
+};
 
 export default Layout;
