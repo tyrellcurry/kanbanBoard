d42480d4afb040d9cd44dfc31886e508d82dbb4d
Tyrell C <tyrellrcurry@gmail.com>
2024-05-18 10:13:38 -0700

    Created DB in docker. Schema and API setup.

--- server/pgadmin/src/main/java/dev/kanban/pgadmin/User.java
+++ server/pgadmin/src/main/java/dev/kanban/pgadmin/User.java
@@ -7,19 +7,17 @@ import java.sql.Timestamp;
 import java.util.List;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(unique = true)
     private String username;
 
-    @Column(nullable = false, unique = true)
+    @Column(unique = true)
     private String email;
-
-    @Column(nullable = false)
-    private String passwordHash;
 
     @CreationTimestamp
     @Column(updatable = false)
@@ -30,6 +28,20 @@ public class User {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Issue> issues;
+
+    protected User() {}
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%d, username='%s', email='%s']",
+                id, username, email);
+    }
 
     // Getters and Setters
 
@@ -55,14 +67,6 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getPasswordHash() {
-        return passwordHash;
-    }
-
-    public void setPasswordHash(String passwordHash) {
-        this.passwordHash = passwordHash;
     }
 
     public Timestamp getCreatedAt() {
