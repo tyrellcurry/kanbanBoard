8b25a35dcb116d280ed3d867e60f12c8d01f6295
Tyrell C <tyrellrcurry@gmail.com>
2024-05-17 21:13:08 -0700

    Basic authentication added via Auth0

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,30 @@
 // src/App.tsx
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 import "./app.scss";
 
-
 const App = () => {
+  const { loginWithRedirect, logout, user, isAuthenticated, isLoading } =
+    useAuth0();
   return (
-    <div>
-      <h1>Hello, React with Webpack!</h1>
-    </div>
+    <main>
+      {isAuthenticated ? (
+        <div>
+          <h1>Hi {user?.name ? user?.name : "there"}, you are signed in!</h1>
+          <button
+            onClick={() =>
+              logout({ logoutParams: { returnTo: window.location.origin } })
+            }>
+            Log Out
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h1>You are not signed in</h1>
+          <button onClick={() => loginWithRedirect()}>Log In</button>
+        </div>
+      )}
+    </main>
   );
 };
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 // src/index.tsx
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
 import Layout from "./Components/Layout";
 import App from "./App";
 import Signin from "./Signin";
@@ -11,9 +12,16 @@ const root = createRoot(domNode);
 
 root.render(
   <>
-    <Layout>
-      <Signin />
-      <App />
-    </Layout>
+    <Auth0Provider
+      domain={process.env.AUTH0_DOMAIN as string}
+      clientId={process.env.AUTH0_CLIENT_ID as string}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}>
+      <Layout>
+        <Signin />
+        <App />
+      </Layout>
+    </Auth0Provider>
   </>
 );
