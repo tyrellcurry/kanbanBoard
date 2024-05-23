da21bdf2c8e35cfe73923131a0366c5ed360eb3f
Tyrell C <tyrellrcurry@gmail.com>
2024-05-22 17:21:31 -0700

    Issue editing menu added

--- client/src/Components/Issue.tsx
+++ client/src/Components/Issue.tsx
@@ -1,14 +1,15 @@
 import React, { useState, FormEvent } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faEdit, faTrash, faEllipsis } from "@fortawesome/free-solid-svg-icons";
 
 const Issue = ({ issue, deleteIssue, updateIssue }: any) => {
   const [editing, setEditing] = useState(false);
+  const [editingMenu, setEditingMenu] = useState(false);
 
   const saveEditedIssue = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
-    const inputText = formData.get('edit') as string;
+    const inputText = formData.getAll("edit");
     updateIssue(issue.id, inputText);
     setEditing(false);
   };
@@ -18,22 +19,31 @@ const Issue = ({ issue, deleteIssue, updateIssue }: any) => {
         <div>
           <form onSubmit={saveEditedIssue}>
             <input name="edit" type="text" data-testid="title_input" />
+            <textarea name="edit" data-testid="title_description"></textarea>
             <button data-testid="save_input">Save</button>
           </form>
         </div>
       ) : (
-        <div>
-          <div>
-          <button data-testid="edit_button" onClick={() => setEditing(true)}>
-          <FontAwesomeIcon icon={faEdit} />
-          </button>
-            <button
-              data-testid="delete_button"
-              onClick={() => deleteIssue(issue.id)}>
-              <FontAwesomeIcon icon={faTrash} />
-            </button>
+        <div className="contents">
+          <div className="edit_menu">
+            <button onClick={() => setEditingMenu(!editingMenu)}><FontAwesomeIcon icon={faEllipsis} /></button>
           </div>
-          <p data-testid="issue_title">{issue.issue}</p>
+          {editingMenu && (
+            <div className="editing">
+              <button
+                data-testid="edit_button"
+                onClick={() => setEditing(true)}>
+                <FontAwesomeIcon icon={faEdit} />
+              </button>
+              <button
+                data-testid="delete_button"
+                onClick={() => deleteIssue(issue.id)}>
+                <FontAwesomeIcon icon={faTrash} />
+              </button>
+            </div>
+          )}
+          <h3 data-testid="issue_title">{issue.issueTitle}</h3>
+          <h3 data-testid="issue_description">{issue.issueDescription}</h3>
         </div>
       )}
     </div>
